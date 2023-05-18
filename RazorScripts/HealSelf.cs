115cad73bf361cee9ae78b731fd01f6660b873c0
Stefan Soderberg <sefan.soderberg@elicit.se>
2023-05-18 20:45:41 +0200

    Swapped to that healing has priority, script order os a bit wonky, but ti's due to an upcoming change

--- RazorScripts/HealSelf.cs
+++ RazorScripts/HealSelf.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using RazorEnhanced;
 namespace Razorscripts
@@ -7,23 +8,48 @@ namespace Razorscripts
         public void Run()
         {
             var journal = new Journal();
+            var counter = 0;
             bool retry =false;
-            do {
+            do
+            {
                 var lastJournal = journal.GetJournalEntry(null).OrderBy(j => j.Timestamp).LastOrDefault();
-                
+
+               
+
                 if (Player.Poisoned)
                 {
+                    if (Player.Hits < 15 && Player.GetRealSkillValue("SpiritSpeak") >= 100)
+                    {
+                        var lastNecro = Misc.ReadSharedValue("HealSelf:LastSS");
+                        //Check if value is not null and is dateTime
+                        if (lastNecro != null && lastNecro is DateTime)
+                        {
+                            var lastNecroTime = (DateTime) lastNecro;
+                            if (lastNecroTime.AddSeconds(5) <= DateTime.Now)
+                            {
+                                Misc.SetSharedValue("HealSelf:LastSS", DateTime.Now);
+                                Player.UseSkill("SpiritSpeak");
+                            }
+                        }
+                        else
+                        {
+                            Misc.SetSharedValue("HealSelf:LastSS", DateTime.Now);
+                            Player.UseSkill("SpiritSpeak");
+                        }
+                    }
+                    
                     Self.Cure();
                 }
                 else
                 {
                     if (Self.NeedHealing)
                     {
-                        Self.Heal();    
+                        Self.Heal();
                     }
                     else
                     {
                         Helper.Log("You are already at full health");
+                        break;
                     }
                 }
 
@@ -32,8 +58,7 @@ namespace Razorscripts
                     retry = true;
                     Misc.Pause(100);
                 }
-            }
-            while (retry);
+            } while (retry && counter++ < 5);
             
             Misc.Pause(300);
             Misc.RemoveSharedValue("Lootmaster:Pause");
@@ -53,14 +78,17 @@ namespace Razorscripts
     {
         private static uint USerial => (uint)Serial;
         public static bool NeedHealing => HitsMax > Hits; 
+        public static readonly bool ForceBands = GetRealSkillValue("Healing") >= 80;
         public static void Cure()
         {
+            
             Misc.SetSharedValue("Lootmaster:Pause", true);
-            if (GetRealSkillValue("Magery") >= 40)
+            
+            if (!ForceBands && GetRealSkillValue("Magery") >= 40)
             {
                 Spells.CastMagery("Cure", USerial);
             }
-            else if (GetRealSkillValue("Chivalry") >= 40)
+            else if (!ForceBands && GetRealSkillValue("Chivalry") >= 40)
             {
                 Spells.CastChivalry("Cleanse By Fire", USerial);
             }
@@ -76,7 +104,7 @@ namespace Razorscripts
         {
             Misc.SetSharedValue("Lootmaster:Pause", true);
             //Todo, check LRC Value in order to validate if the player might have died and won't have regs, if so check for Spirit Speak Skill to heal
-            if (GetRealSkillValue("Magery") > 30)
+            if (!ForceBands && GetRealSkillValue("Magery") > 30)
             {
                 if (GetRealSkillValue("Magery") > 60 && HitsMax - Hits > 20)
                 {
@@ -89,7 +117,7 @@ namespace Razorscripts
                 }
                 
             }
-            else if (GetRealSkillValue("Chivalry") > 60 && HitsMax - Hits > 20)
+            else if (!ForceBands && GetRealSkillValue("Chivalry") > 60 && HitsMax - Hits > 20)
             {
                 Spells.CastChivalry("Close Wounds", USerial);
             }
