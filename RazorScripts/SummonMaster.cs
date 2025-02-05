8a8cad0b810457d552d77d8fe5196836b618c570
Matt Dorana <matrim@usbanime.com>
2025-02-05 09:45:03 +0100

    Rewrite of SummonMaster, now works smoother and has timers

--- RazorScripts/SummonMaster.cs
+++ RazorScripts/SummonMaster.cs
@@ -12,48 +12,53 @@ namespace RazorScripts
         private uint Gumpid = 98413566;
         private List<Mobile> Summons = new List<Mobile>();
         private List<MockMob> LastLoop = new List<MockMob>();
-        private List<int> SummonsSerials = new List<int>();
-        private Journal jurnal = new Journal();
-        private Journal.JournalEntry _last = null;
+        private List<int> SerialsLog = new List<int>();
+        Dictionary<int,DateTime> _timers = new Dictionary<int, DateTime>();
+        System.Timers.Timer _timer = new System.Timers.Timer(5000);
+        private Target _target = new Target();
+        private string _version = "1.2.0";
 
         public void Run()
         {
-            _last = jurnal.GetJournalEntry(null).OrderBy(j => j.Timestamp).LastOrDefault();
             UpdateGump();
             try
             {
+                _timer.Enabled = true;
+                _timer.Elapsed += (sender, e) => UpdateGump();
+                _timer.AutoReset = true;
+                _timer.Start();
+                
                 while (true)
                 {
-                    var entries = jurnal.GetJournalEntry(_last);
                     var newSummons = false;
-                    if (entries.Any(CheckSummons))
-                    {
-                        var precast = Mobiles.ApplyFilter(new Mobiles.Filter
-                        {
-                            RangeMax = 20,
-                            RangeMin = 0,
-                            Notorieties = new List<byte> { 1,2 },
-                        });
-
-                        newSummons = WaitForSummons(precast);
-
-                        _last = entries.OrderBy(e => e.Timestamp).LastOrDefault();
-                    }
 
                     var runMobs = Mobiles.ApplyFilter(new Mobiles.Filter
                     {
                         RangeMax = 20,
                         RangeMin = 0,
-                        Notorieties = new List<byte> { 1,2 },
-                    }).Where(m => SummonsSerials.Contains(m.Serial)).ToList();
+                        Notorieties = new List<byte> { 1, 2 },
+                    }).ToList();
+                    
                     runMobs.ForEach(m => Mobiles.WaitForProps(m, 1000));
                     var runSums = runMobs.Where(m => m.Properties.Any(p => p.Number == 1049646)).ToList();
-                    Summons = runSums;
+                    var mySumms = FilterMySummons(runSums);
+                    Summons = mySumms;
+                    foreach (var summon in Summons)
+                    {
+                        if (SerialsLog.Contains(summon.Serial))
+                        {
+                            continue;
+                        }
+                        
+                        SerialsLog.Add(summon.Serial);
+                        newSummons = true;
+                    }
                     var change = false;
                     if (newSummons)
                     {
-                       GuardMode();
+                        GuardMode();
                     }
+
                     var reply = Gumps.GetGumpData(Gumpid);
                     if (reply.buttonid != -1)
                     {
@@ -69,9 +74,17 @@ namespace RazorScripts
                                 ReleaseAll();
                                 reply.buttonid = -1;
                                 break;
-                            case SumReply.Summon:
-                                UpdateGump();
-                                SummonFey();
+                            case SumReply.Attack:
+                                var targetSerial = _target.PromptTarget();
+                                
+                                if(targetSerial != 0)
+                                {
+                                    var mob = Mobiles.FindBySerial(targetSerial);
+                                    if (mob != null)
+                                    {
+                                        Attack(mob);
+                                    }
+                                }
                                 reply.buttonid = -1;
                                 break;
                             default:
@@ -135,46 +148,38 @@ namespace RazorScripts
                         }));
                     }
 
-                    Misc.Pause(100);
+                    Misc.Pause(500);
                 }
+            }
+            catch (ThreadAbortException)
+            {
+                //silent
             }
             catch (Exception e)
             {
-                if (e.GetType() != typeof(ThreadAbortException))
+                Misc.SendMessage(e.ToString());
+            }
+            finally
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                Gumps.CloseGump(Gumpid);
+            }
+
+        }
+
+        private List<Mobile> FilterMySummons(List<Mobile> summons)
+        {
+            var result = new List<Mobile>();
+            foreach (var mob in summons)
+            {
+                if(mob.CanRename)
                 {
-                    Misc.SendMessage(e.ToString());
+                    result.Add(mob);
                 }
             }
 
-        }
-
-        private bool CheckSummons(Journal.JournalEntry entry)
-        {
-            if(entry.Name != Player.Name)
-            {
-                return false;
-            }
-
-            return entry.Text.ToLower().Contains("alalithra")
-                   || entry.Text.ToLower().Contains("kal vas xen corp")
-                   || entry.Text.ToLower().Contains("kal vas xen hur")
-                   || entry.Text.ToLower().Contains("kal vas xen ylem")
-                   || entry.Text.ToLower().Contains("kal vas xen flam")
-                   || entry.Text.ToLower().Contains("kal vas xen an flam")
-                   || entry.Text.ToLower().Contains("in jux por ylem")
-                   || entry.Text.ToLower().Contains("kal vas cen Corp ylem")
-                   || entry.Text.ToLower().Contains("kal xen");
-        }
-
-        private void SummonFey()
-        {
-            var precast = Mobiles.ApplyFilter(new Mobiles.Filter
-            {
-                RangeMax = 2,
-                RangeMin = 0,
-                Notorieties = new List<byte> { 1,2 },
-            });
-            Spells.CastSpellweaving("Summon Fey");
+            return result;
         }
 
         private void GuardMode()
@@ -186,6 +191,16 @@ namespace RazorScripts
                 if (sumProp == null || !sumProp.ToString().Contains("summoned")) continue;
                 Misc.WaitForContext(mob, 500);
                 Misc.ContextReply(mob, 2);
+                var timeprop = mob.Properties.FirstOrDefault(p => p.Number == 1060847);
+                if(timeprop != null)
+                {
+                    var parts = timeprop.Args.Split('\t')[1].Split(':');
+                    if (parts.Length == 2)
+                    {
+                        var time = DateTime.Now.AddMinutes(int.Parse(parts[0])).AddSeconds(int.Parse(parts[1]));
+                        _timers[mob.Serial] = time;
+                    }
+                }
             }
         }
 
@@ -208,6 +223,29 @@ namespace RazorScripts
                 }
             }
         }
+        
+        private void Attack(Mobile target)
+        {
+            var filter = new Mobiles.Filter
+            {
+                RangeMax = 20,
+                RangeMin = 0,
+                Notorieties = new List<byte> { 1, 2 }
+            };
+
+            var mobs = Mobiles.ApplyFilter(filter);
+            foreach (var mob in mobs)
+            {
+                Mobiles.WaitForProps(mob,1000);
+                if (mob.Properties.Any(p => p.Number == 1049646))
+                {
+                    Misc.WaitForContext(mob, 500);
+                    Misc.ContextReply(mob, 0);
+                    Target.WaitForTarget(1000);
+                    Target.TargetExecute(target);
+                }
+            }
+        }
 
         private void ReleaseSummon(int serial)
         {
@@ -218,92 +256,44 @@ namespace RazorScripts
                 Misc.ContextReply(mob, 5);
             }
         }
-        private bool WaitForSummons(List<Mobile> summons)
-        {
-            Misc.SendMessage("Waiting for Summons to appear");
-            var rems = new List<int>();
-            foreach (var serial in SummonsSerials.Where(serial => Summons.All(s => s.Serial != serial)))
-            {
-                rems.Add(serial);
-            }
-            rems.ForEach(s => SummonsSerials.Remove(s));
-
-            var clockStart = DateTime.Now;
-            while (DateTime.Now < clockStart.AddSeconds(5))
-            {
-                var postcast = Mobiles.ApplyFilter(new Mobiles.Filter
-                {
-                    RangeMax = 2,
-                    RangeMin = 0,
-                    Notorieties = new List<byte> { 1,2 },
-                });
-            
-                //Find Mobiles only in postcase
-                var newMobs = postcast.Where(m => summons.All(p => p.Serial != m.Serial)).ToList();
-                if (newMobs.Any())
-                {
-                    newMobs.ForEach(m => SummonsSerials.Add(m.Serial));
-                    return true;
-                }
-                Misc.Pause(50);
-            }
-
-            return false;
-        }
 
         private void UpdateGump()
         {
             var sumGump = Gumps.CreateGump();
             sumGump.gumpId = Gumpid;
             sumGump.serial = (uint)Player.Serial;
-            Gumps.AddBackground(ref sumGump,0,0,480,120,-1);
-            
-            Gumps.AddImage(ref sumGump,0,0,9400);
-            for (var i = 1; i <= 40; i++)
-            {
-                Gumps.AddImage(ref sumGump,i*11,0,9401);
-            }
-            Gumps.AddImage(ref sumGump,451,0,9402);
-            for (var row = 1; row <= 38; row++)
-            {
-                Gumps.AddImage(ref sumGump,0,row*2+11,9403);
-            }
-            
-            Gumps.AddImage(ref sumGump,0,89,9406);
-            for (var i = 1; i <= 40; i++)
-            {
-                Gumps.AddImage(ref sumGump,i*11,89,9407);
-            }
-            Gumps.AddImage(ref sumGump,451,89,9408);
-            
-            for (var row = 1; row <= 38; row++)
-            {
-                Gumps.AddImage(ref sumGump,451,row*2+11,9405);
-            }
-            Gumps.AddImageTiled(ref sumGump,11,11,450,80, 9404);
+            Gumps.AddBackground(ref sumGump,0,0,480,120,1755);
 
             foreach (var sum in Summons)
             {
                 var index = Summons.IndexOf(sum) + 1;
-                Gumps.AddButton(ref sumGump,index*60-40,20,GetGumpKey(sum),GetGumpKey(sum),sum.Serial,1,1);
+                Gumps.AddButton(ref sumGump,index*60-40,30,GetGumpKey(sum),GetGumpKey(sum),sum.Serial,1,1);
                 Gumps.AddTooltip(ref sumGump,"Release " + sum.Name);
                 if (sum.Properties.Any(p => p.Number == 1080078))
                 {
-                    Gumps.AddLabel(ref sumGump, index*60-40+5,65,0x35,"Guard");
+                    Gumps.AddLabel(ref sumGump, index*60-40+5,75,0x35,"Guard");
                 }
                 if (sum.WarMode)
                 {
-                    Gumps.AddLabel(ref sumGump, index*60-40+2,80,0x25,"Combat");
+                    Gumps.AddLabel(ref sumGump, index*60-40+2,90,0x25,"Combat");
+                }
+                if(_timers.ContainsKey(sum.Serial))
+                {
+                    var time = _timers[sum.Serial];
+                    var diff = time - DateTime.Now;
+                    Gumps.AddLabel(ref sumGump, index*60-40+5,10,0x35,$"{diff.Minutes}:{diff.Seconds.ToString("##")}");
                 }
             }
             
-            Gumps.AddButton(ref sumGump, 330,5,9721,9722,(int)SumReply.Summon,1,0);
-            Gumps.AddButton(ref sumGump, 330,35,9721,9722,(int)SumReply.Release,1,0);
-            Gumps.AddButton(ref sumGump, 330,65,9721,9722,(int)SumReply.Guard,1,0);
-            Gumps.AddLabel(ref sumGump, 370,10,0x30,"Summon");
-            Gumps.AddLabel(ref sumGump, 370,40,0x6D,"Release");
-            Gumps.AddLabel(ref sumGump, 370,70,0x35,"Guard");
+            Gumps.AddButton(ref sumGump, 330,10,9903,9904,(int)SumReply.Attack,1,0);
+            Gumps.AddButton(ref sumGump, 330,40,9903,9904,(int)SumReply.Release,1,0);
+            Gumps.AddButton(ref sumGump, 330,70,9903,9904,(int)SumReply.Guard,1,0);
+            Gumps.AddLabel(ref sumGump, 355,10,0x30,"Attack");
+            Gumps.AddLabel(ref sumGump, 355,40,0x6D,"Release");
+            Gumps.AddLabel(ref sumGump, 355,70,0x35,"Guard");
             
+            Gumps.AddLabel(ref sumGump,290,95,0x7b, "Summon Master");
+            Gumps.AddLabel(ref sumGump,393,95,0x7b, "Version: " + _version);
             
             Gumps.CloseGump(Gumpid);
             Gumps.SendGump(sumGump,500,500);
@@ -325,6 +315,10 @@ namespace RazorScripts
                     return 2302;
                 case 0x0010:
                     return 2303;
+                case 0x033D:
+                    return 24015;
+                case 0x02B4:
+                    return 24006;
                 default :
                     return 2279;
             }
@@ -332,7 +326,7 @@ namespace RazorScripts
         
         private enum SumReply
         {
-            Summon = 1,
+            Attack = 1,
             Release = 2,
             Guard = 3
         }
